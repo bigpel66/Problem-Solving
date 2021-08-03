82b5c55777b34ebd663558c0e54e4fa128cca8bb
BIGPEL <bigpel66@gmail.com>
2021-08-03 16:36:03 +0900

    a lot problems

--- boj/silver/boj_10989.cpp
+++ boj/silver/boj_10989.cpp
@@ -2,8 +2,6 @@
 #include <vector>
 
 std::vector<int> g_numbers;
-int				g_min;
-int				g_max;
 
 void	pre_setting(void)
 {
@@ -17,34 +15,26 @@ void	input_action(void)
 	int	iter;
 
 	std::cin >> iter;
-	g_numbers = std::vector<int>(iter, 0);
-	for (auto& i:g_numbers)
-		std::cin >> i;
-	g_min = 2147483647;
-	g_max = -2147483648;
-}
-
-void	solution(void)
-{
-	for (auto& i:g_numbers)
+	g_numbers = std::vector<int>(10000, 0);
+	for (int i = 0; i < iter; ++i)
 	{
-		if (g_min > i)
-			g_min = i;
-		if (g_max < i)
-			g_max = i;
+		int	val;
+		std::cin >> val;
+		++(g_numbers.at(val - 1));
 	}
 }
 
 void	output_action(void)
 {
-	std::cout << g_min << ' ' << g_max;
+	for (int i = 0; i < 10000; ++i)
+		for (int j = 0; j < g_numbers[i]; ++j)
+			std::cout << i + 1<< '\n';
 }
 
 int		main(void)
 {
 	pre_setting();
 	input_action();
-	solution();
 	output_action();
 	return (0);
 }
